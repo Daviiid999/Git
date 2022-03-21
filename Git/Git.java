e75426e39ce7da0eb12f9e3b520def702ebfe608
Daviiid999 <david.luque@itponiente.com>
2022-03-21 09:19:29 +0100

    Comandos

---
+++ Git/Git.java
@@ -0,0 +1,9 @@
+
+1. Entro en la carpeta que quiero que se convierta usando comandos de terminal.
+2.1 Crear un repositorio.
+  - git init
+2.2. Clonar uno que ya exista.
+  - git clone "url"
+3. Para ver el estado de mi repositorio.
+  - git status
+    Me dice si hay 
